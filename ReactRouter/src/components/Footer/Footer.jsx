db4747f333d7bd6a0ec6fc964c58ea1d49d49437
utkarsh dixit <utkarshdixit210@gmail.com>
2024-11-09 22:07:54 +0530

    commit

--- ReactRouter/src/components/Footer/Footer.jsx
+++ ReactRouter/src/components/Footer/Footer.jsx
@@ -71,9 +71,9 @@ export default function Footer() {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
-                        </a>
+                        
+                            Utkarsh Dixit
+                       
                         . All Rights Reserved.
                     </span>
                     <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
